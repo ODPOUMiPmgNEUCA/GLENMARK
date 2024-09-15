6a732a9ca9dfb6b7d24093d30397117b013b4b47
julia <168024513+juliraven@users.noreply.github.com>
2024-09-15 20:53:08 +0200

    Update GLENMARK.py

--- GLENMARK.py
+++ GLENMARK.py
@@ -131,7 +131,7 @@ liczba_duplikatow
 st.write('Liczba wszystkich wierszy :')
 st.write(len(df_dopasowany))
 st.write('Liczba wierszy, w których został dopasowany kod, ale nie ma w liście apteki, która ma taki kod :')
-st.write(df_dopasowany['Nazwa apteki_dopasowany'].isna().sum())
+st.write(df_dopasowany['Nazwa apteki'].isna().sum())
 st.write('Procent dopasowania :')
 st.write(df_dopasowany['Nazwa apteki_dopasowany'].isna().sum()/len(df_dopasowany) * 100,'%')
 
