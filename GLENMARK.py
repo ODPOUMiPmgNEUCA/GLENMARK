e76aa77e8a614f3eca866408989b72e05d35dc3a
julia <168024513+juliraven@users.noreply.github.com>
2024-10-07 11:19:38 +0200

    Update GLENMARK.py

--- GLENMARK.py
+++ GLENMARK.py
@@ -55,7 +55,7 @@ if df_file:
 
         lista_unique = lista.drop_duplicates(subset=['Kod pocztowy'])
         df1 = df1.merge(lista_unique[['Kod pocztowy','SAP','Nazwa apteki','Miejscowość','Ulica','Nr domu']], on='Kod pocztowy', how='left')
-        df1[df1['Kod pocztowy'=='00-022']]
+        df1[df1['Kod pocztowy'==00-022]]
       
         # Wszystkie dostępne kody :
         kody = lista['Kod pocztowy'].unique().tolist()
